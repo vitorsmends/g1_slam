4400da6f33140b54b76517301fb0ea7bc8b11b68
mendes <vitorsmendscod@gmail.com>
2026-02-13 14:18:32 -0300

    add config paths to setup

--- setup.py
+++ setup.py
@@ -9,21 +9,29 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        # ament index resource (must exist as a file: resource/g1_slam)
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
+
+        # package manifest
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
+
+        # launch files (*.launch.py)
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*.launch.py'))),
+
+        # config files (*.yaml)
+        (os.path.join('share', package_name, 'config'),
+         glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='mendes',
     maintainer_email='vitormendesrb@gmail.com',
-    description='Package to bring up SLAM for G1 robot using Livox LiDAR',
+    description='Bringup package for SLAM and localization on the Unitree G1 using Livox LiDAR',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
