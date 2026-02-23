fbb87b434a3f849831882d07067657cc9d416d3f
mendes <vitorsmendscod@gmail.com>
2026-02-23 13:06:19 -0300

    fix build script

--- build.sh
+++ build.sh
@@ -3,13 +3,4 @@ set -e
 
 IMAGE_NAME=g1_slam:humble
 
-ROS_DOMAIN_ID=0
-CYCLONEDDS_HOME=/lib/cyclonedds/install
-
-docker run -it --rm \
-  --name g1_slam \
-  --net=host \
-  --ipc=host \
-  -e ROS_DOMAIN_ID=${ROS_DOMAIN_ID} \
-  -e CYCLONEDDS_HOME=${CYCLONEDDS_HOME} \
-  ${IMAGE_NAME}
+docker build -t "${IMAGE_NAME}" .
