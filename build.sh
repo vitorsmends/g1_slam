137130f3a88bd80acb6cf0ed120d52081c1937ab
mendes <vitorsmendscod@gmail.com>
2026-02-23 12:31:53 -0300

    feat: enable DDS client (CycloneDDS + Unitree SDK)

--- build.sh
+++ build.sh
@@ -3,4 +3,13 @@ set -e
 
 IMAGE_NAME=g1_slam:humble
 
-docker build -t ${IMAGE_NAME} .
+ROS_DOMAIN_ID=0
+CYCLONEDDS_HOME=/lib/cyclonedds/install
+
+docker run -it --rm \
+  --name g1_slam \
+  --net=host \
+  --ipc=host \
+  -e ROS_DOMAIN_ID=${ROS_DOMAIN_ID} \
+  -e CYCLONEDDS_HOME=${CYCLONEDDS_HOME} \
+  ${IMAGE_NAME}
