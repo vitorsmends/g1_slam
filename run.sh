fee4513c74e7a897889293e69a0579b74dba5967
mendes <vitorsmendscod@gmail.com>
2026-03-16 18:51:26 -0300

    fix docker run script

--- run.sh
+++ run.sh
@@ -5,10 +5,14 @@ set -e
 IMAGE_NAME=g1_slam:humble
 ROS_DOMAIN_ID=0
 
-docker run -d --rm \
+docker rm -f g1_slam 2>/dev/null || true
+
+docker run -d \
   --name g1_slam \
   --net=host \
   --ipc=host \
   -e ROS_DOMAIN_ID=${ROS_DOMAIN_ID} \
   ${IMAGE_NAME} \
-  /ros_entrypoint.sh
+  bash -c "source /opt/ros/humble/setup.bash && \
+           source /opt/ws/install/setup.bash && \
+           ros2 launch g1_slam slam.launch.py"
