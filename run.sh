1d1a7faa0828d75d565db129eb9ed7b5033fdc72
mendes <vitorsmendscod@gmail.com>
2026-03-19 14:59:16 -0300

    fix run sh

--- run.sh
+++ run.sh
@@ -1,15 +1,9 @@
-#!/usr/bin/env bash
-
-IMAGE_NAME=g1_slam:humble
-ROS_DOMAIN_ID=0
-
-docker rm -f g1_slam 2>/dev/null || true
-
 docker run -d \
   --name g1_slam \
   --restart unless-stopped \
   --net=host \
   --ipc=host \
   -e ROS_DOMAIN_ID=${ROS_DOMAIN_ID} \
+  -v ~/g1_slam:/opt/ws \
   ${IMAGE_NAME} \
-  /ros_entrypoint.sh ros2 launch g1_slam slam.launch.py
+  bash -c "source /opt/ros/humble/setup.bash && cd /opt/ws && colcon build && source install/setup.bash && ros2 launch g1_slam slam.launch.py"
